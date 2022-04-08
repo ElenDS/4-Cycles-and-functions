27a1afedcfa023f89e76e97f5eb11a009bdcb064
AnelD <dolena0308@gmail.com>
2022-04-08 20:01:51 +0300

    edit main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+for( let i = 0 ; i >= 0 ; i++ ){
+    
+    let userWord = prompt("Ваше слово?");
+    
+    if(userWord==="break"){
+        break;
+    } else if(userWord==="continue"){
+        continue;
+    }
+    
+    alert(i);
+}
